63754875f6b477e8969894c690b19cd11260bd8f
datathug <eugenekalinouski@gmail.com>
2026-05-12 10:59:55 +0200

    Deploy dev branch to /dev subfolder on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/terroirthattravelsmaps/',
+  base: process.env.VITE_BASE_PATH ?? '/terroirthattravelsmaps/',
 });
